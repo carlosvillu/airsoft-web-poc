dc65784d94e62090e25714e5ba6b53739f08ea44
Glitch (hello-webpage) <none>
2020-11-26 10:24:19 +0000

    🚛☎️ Checkpoint ./index.html:460023/2275 ./script.js:460023/1443 ./.prettierrc.js:460023/480 ./.prettierrc.json:460023/279

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+import { render } from "https://npm.reversehttp.com/preact,preact/hooks,htm/preact";
 
-// prints "hi" in the browser's dev tools console
 console.log("hi");
