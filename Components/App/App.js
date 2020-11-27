b7f21a9ebf95ed99546e9bc1c68f19f51a91bed7
Glitch (hello-webpage) <none>
2020-11-27 14:37:27 +0000

    😇🌮 Checkpoint ./Components/App/App.js:460023/596

--- Components/App/App.js
+++ Components/App/App.js
@@ -1,9 +1,12 @@
-import { useState, createElement as c } from 'https://npm.reversehttp.com/preact,preact/hooks,htm/preact'
+import {
+  useState,
+  createElement as c,
+} from 'https://npm.reversehttp.com/preact,preact/hooks,htm/preact'
 import { Router } from 'https://npm.reversehttp.com/preact-router'
 
 const App = () => {
   const [name, setName] = useState('Carlitos')
-  return c('h1', null, 'Hola!')
+  return c('h1', null, name)
 }
 
 export { App }
