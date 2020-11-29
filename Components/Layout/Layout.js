b784b39ce9a18d87e53a2c8f79cee4b8c32045ce
Carlos Villuendas <carlosvillu@gmail.com>
2020-11-29 12:56:13 +0100

    feat(Layout): make responsive

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -6,19 +6,33 @@ import {Navigation} from '../Navigation/Navigation.js'
 const Layout = ({children}) => {
   return html`
     <section
-      class="d_grid gta-holy-layout gtc-holy-layout gtr-holy-layout gg-holy-layout h_100vh"
+      class="
+        d_grid
+        gta-holy-layout
+        gtc-holy-layout
+        gtr-holy-layout
+        gg-holy-layout
+        h_100vh
+        tld__gta-holy-layout
+        tld__gtc-holy-layout
+        d__gta-holy-layout
+        d__gtc-holy-layout"
     >
       <header class="debug ga_header h-s">
         <${Topbar} />
       </header>
 
-      <nav class="ga_nav debug">
+      <nav class="ga_nav d_none tld__d_block debug">
         <${Navigation} />
       </nav>
 
       <main class="ga_content debug">
         ${children}
       </main>
+
+      <aside class="ga_side d_none d__d_block debug">
+        ASIDE
+      </aside>
 
       <footer class="ga_footer h-s debug">
         <${Footer} />
