be7c8d4004dc0fdd4b26bc02c14c582ad7c136f8
Joan León | @nucliweb <joan.leon@gmail.com>
2020-12-17 19:57:29 +0100

    feat(Debug): random border color in debug mode

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ import {
 
 import {App} from './Components/App/App.js'
 
+const DEBUG = true
+
+if (DEBUG) {
+  document.documentElement.style.setProperty(
+    '--c-debug',
+    `hsl(${360 * Math.random()}, 80%, 40%)`
+  )
+}
 render(
   html`
     <${App} />
